94e2aed99fbcf47b7136506ed40feb4782fb1f89
Luc Khai Hai <lkh42t@gmail.com>
2021-07-17 02:32:42 +0900

    [nvim] reformat with stylua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,42 +1,42 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+  execute("packadd packer.nvim")
 end
 
-require'packer'.startup(function(use)
-  use 'wbthomason/packer.nvim'
+require("packer").startup(function(use)
+  use("wbthomason/packer.nvim")
 
   -- editor scheme
-  use 'fneu/breezy'
-  use 'itchyny/lightline.vim'
+  use("fneu/breezy")
+  use("itchyny/lightline.vim")
 
   -- language support
-  use 'JuliaEditorSupport/julia-vim'
-  use 'dart-lang/dart-vim-plugin'
+  use("JuliaEditorSupport/julia-vim")
+  use("dart-lang/dart-vim-plugin")
 
   -- editor enhancement
-  use 'editorconfig/editorconfig-vim'
-  use 'airblade/vim-gitgutter'
-  use 'tpope/vim-fugitive'
-  use 'preservim/nerdcommenter'
-  use 'cohama/lexima.vim'
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use 'ctrlpvim/ctrlp.vim'
+  use("editorconfig/editorconfig-vim")
+  use("airblade/vim-gitgutter")
+  use("tpope/vim-fugitive")
+  use("preservim/nerdcommenter")
+  use("cohama/lexima.vim")
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("ctrlpvim/ctrlp.vim")
 
   -- language server and completion
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/vim-vsnip-integ'
+  use("neovim/nvim-lspconfig")
+  use("hrsh7th/nvim-compe")
+  use("hrsh7th/vim-vsnip")
+  use("hrsh7th/vim-vsnip-integ")
 end)
 
-if fn.empty(fn.glob(fn.stdpath('config') .. '/plugin/packer_compiled.lua')) then
-  vim.cmd [[PackerCompile]]
+if fn.empty(fn.glob(fn.stdpath("config") .. "/plugin/packer_compiled.lua")) then
+  vim.cmd([[PackerCompile]])
 end
 
-vim.cmd [[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]]
+vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,114 +1,121 @@
-local lspconfig = require 'lspconfig'
+local lspconfig = require("lspconfig")
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
-  vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
+  vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-  local opts = {noremap = true, silent = true}
+  local opts = { noremap = true, silent = true }
 
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  buf_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "<leader>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("n", "<leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {'documentation', 'detail', 'additionalTextEdits'},
+  properties = { "documentation", "detail", "additionalTextEdits" },
 }
 
 -- {{{ language servers without specific settings
 local servers = {
-  'bashls',
-  'clangd',
-  'cmake',
-  'dartls',
-  'gopls',
-  'jedi_language_server',
-  'texlab',
-  'rust_analyzer',
+  "bashls",
+  "clangd",
+  "cmake",
+  "dartls",
+  "gopls",
+  "jedi_language_server",
+  "texlab",
+  "rust_analyzer",
 }
 for _, server in ipairs(servers) do
-  lspconfig[server].setup {on_attach = on_attach, capabilities = capabilities}
+  lspconfig[server].setup({ on_attach = on_attach, capabilities = capabilities })
 end
 -- }}}
 
 -- {{{ diagnostic-languageserver
-lspconfig.diagnosticls.setup {
+lspconfig.diagnosticls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {'lua', 'python', 'sh', 'zsh'},
+  filetypes = { "lua", "python", "sh", "zsh" },
   init_options = {
     -- https://github.com/iamcco/diagnostic-languageserver/wiki/Linters
     linters = {
       flake8 = {
-        command = 'flake8',
+        command = "flake8",
         debounce = 100,
-        args = {'--format=%(row)d,%(col)d,%(code).1s,%(code)s: %(text)s', '-'},
+        args = { "--format=%(row)d,%(col)d,%(code).1s,%(code)s: %(text)s", "-" },
         offsetLine = 0,
         offsetColumn = 0,
-        sourceName = 'flake8',
+        sourceName = "flake8",
         formatLines = 1,
         formatPattern = {
           [[(\d+),(\d+),([A-Z]),(.*)(\r|\n)*$]],
-          {line = 1, column = 2, security = 3, message = 4},
+          { line = 1, column = 2, security = 3, message = 4 },
         },
-        security = {W = 'warning', E = 'error', F = 'error', C = 'error', N = 'error'},
+        security = { W = "warning", E = "error", F = "error", C = "error", N = "error" },
       },
     },
     filetypes = {
-      python = 'flake8',
+      python = "flake8",
     },
     -- https://github.com/iamcco/diagnostic-languageserver/wiki/Formatters
     formatters = {
       black = {
-        command = 'black',
-        args = {'--quiet', '-'}
+        command = "black",
+        args = { "--quiet", "-" },
       },
       isort = {
-        command = 'isort',
-        args = {'--quiet', '-'},
+        command = "isort",
+        args = { "--quiet", "-" },
       },
       shfmt = {
-        command = 'shfmt',
+        command = "shfmt",
+      },
+      stylua = {
+        command = "stylua",
+        args = { "-s", "-" },
       },
     },
     formatFiletypes = {
-      python = {'isort', 'black'},
-      sh = 'shfmt',
-      zsh = 'shfmt',
+      lua = "stylua",
+      python = { "isort", "black" },
+      sh = "shfmt",
+      zsh = "shfmt",
     },
   },
-}
+})
 -- }}}
 
 -- {{{ Lua language server
 local system_name
-if vim.fn.has('mac') > 0 then
-  system_name = 'macOS'
-elseif vim.fn.has('unix') > 0 then
-  system_name = 'Linux'
+if vim.fn.has("mac") > 0 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") > 0 then
+  system_name = "Linux"
 end
 
-local sumneko_root = vim.fn.stdpath('cache') .. '/lspconfig/sumneko_lua/lua-language-server'
-local sumneko_bin = sumneko_root .. '/bin/' .. system_name .. '/lua-language-server'
+local sumneko_root = vim.fn.stdpath("cache") .. "/lspconfig/sumneko_lua/lua-language-server"
+local sumneko_bin = sumneko_root .. "/bin/" .. system_name .. "/lua-language-server"
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
-lspconfig.sumneko_lua.setup {
+lspconfig.sumneko_lua.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {sumneko_bin, '-E', sumneko_root .. '/main.lua'},
+  cmd = { sumneko_bin, "-E", sumneko_root .. "/main.lua" },
   settings = {
     Lua = {
-      runtime = {version = 'LuaJIT', path = runtime_path},
-      diagnostics = {globals = {'vim'}},
-      workspace = {library = vim.api.nvim_get_runtime_file('', true)},
+      runtime = { version = "LuaJIT", path = runtime_path },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
     },
   },
-}
+})
 -- }}}
