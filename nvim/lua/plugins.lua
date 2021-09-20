8ff6d733c1b5676265f7d3648114a3c8ca8e6474
Luc Khai Hai <lkh42t@gmail.com>
2021-09-20 21:38:57 +0900

    [nvim] fix

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,6 @@
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-if vim.fn.empty(vim.fn.glob(install_path)) then
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
   vim.cmd([[packadd packer.nvim]])
 end
@@ -43,7 +43,7 @@ require("packer").startup(function(use)
   })
 end)
 
-if vim.fn.empty(vim.fn.glob(vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua")) then
+if vim.fn.empty(vim.fn.glob(vim.fn.stdpath("config") .. "/plugin/packer_compiled.lua")) > 0 then
   vim.cmd([[PackerCompile]])
 end
 
