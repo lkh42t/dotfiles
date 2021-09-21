c326312c8b5b90502b41279917e24792b6bef61b
Luc Khai Hai <lkh42t@gmail.com>
2021-09-22 00:58:14 +0900

    [zsh] support macos

---
+++ zsh/platforms/darwin.zsh
@@ -0,0 +1,14 @@
+LS_COLOR_OPT='-G'
+GREP_COLOR_OPT='--color=auto'
+DIFF_COLOR_OPT=''
+
+SYNTAX_HIGHLIGHT_SRC="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+GIT_PS1_SRC=/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+
+unload_variables() {
+	unset LS_COLOR_OPT GREP_COLOR_OPT DIFF_COLOR_OPT
+	unset SYNTAX_HIGHLIGHT_SRC
+	unset GIT_PS1_SRC
+	unset -f unload_variables
+}
