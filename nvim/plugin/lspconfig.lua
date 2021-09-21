9ebec7398241f453a2096631490176ed406a4cf7
Luc Khai Hai <lkh42t@gmail.com>
2021-09-21 16:36:52 +0900

    [nvim] update lsp configs

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,6 +1,7 @@
 local lspconfig = require("lspconfig")
 
-local function on_attach(client, bufnr)
+-- {{{ on_attach
+local function on_attach(_, bufnr)
   local function buf_set_keymap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
@@ -10,122 +11,146 @@ local function on_attach(client, bufnr)
   local opts = { noremap = true, silent = true }
 
   buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "<Leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   buf_set_keymap("n", "<Leader>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("v", "<Leader>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   buf_set_keymap("n", "<Leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
   buf_set_keymap("n", "<Leader>gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<Leader>gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
   buf_set_keymap("n", "<Leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<Leader>td", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 end
+-- }}}
 
+-- {{{ create new capabilities to enable snippets
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = { "documentation", "detail", "additionalTextEdits" },
-}
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+-- }}}
 
--- {{{ language servers without specific settings
+-- {{{ language servers
 local servers = {
-  "bashls",
-  "clangd",
-  "cmake",
-  "dartls",
-  "gopls",
-  "jedi_language_server",
-  "rust_analyzer",
-  "texlab",
-}
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({ on_attach = on_attach, capabilities = capabilities })
-end
--- }}}
-
--- {{{ diagnostic-languageserver
-lspconfig.diagnosticls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "lua", "python", "sh", "zsh" },
-  init_options = {
-    -- https://github.com/iamcco/diagnostic-languageserver/wiki/Linters
-    linters = {
-      flake8 = {
-        command = "flake8",
-        debounce = 100,
-        args = { "--format=%(row)d,%(col)d,%(code).1s,%(code)s: %(text)s", "-" },
-        offsetLine = 0,
-        offsetColumn = 0,
-        sourceName = "flake8",
-        formatLines = 1,
-        formatPattern = {
-          [[(\d+),(\d+),([A-Z]),(.*)(\r|\n)*$]],
-          { line = 1, column = 2, security = 3, message = 4 },
+  bashls = {},
+  clangd = {
+    cmd = {
+      "clangd",
+      "--all-scopes-completion",
+      "--background-index",
+      "--clang-tidy",
+      "--completion-style=detailed",
+      "--cross-file-rename",
+      "--header-insertion=iwyu",
+    },
+  },
+  cmake = {},
+  dartls = {},
+  diagnosticls = {
+    filetypes = { "lua", "python", "sh", "zsh" },
+    init_options = {
+      -- https://github.com/iamcco/diagnostic-languageserver/wiki/Linters
+      linters = {
+        flake8 = {
+          command = "flake8",
+          debounce = 100,
+          args = { "--format=%(row)d,%(col)d,%(code).1s,%(code)s: %(text)s", "-" },
+          offsetLine = 0,
+          offsetColumn = 0,
+          sourceName = "flake8",
+          formatLines = 1,
+          formatPattern = {
+            [[(\d+),(\d+),([A-Z]),(.*)(\r|\n)*$]],
+            { line = 1, column = 2, security = 3, message = 4 },
+          },
+          security = { W = "warning", E = "error", F = "error", C = "error", N = "error" },
         },
-        security = { W = "warning", E = "error", F = "error", C = "error", N = "error" },
       },
-    },
-    filetypes = {
-      python = "flake8",
-    },
-    -- https://github.com/iamcco/diagnostic-languageserver/wiki/Formatters
-    formatters = {
-      black = {
-        command = "black",
-        args = { "-q", "-" },
+      filetypes = {
+        python = "flake8",
       },
-      isort = {
-        command = "isort",
-        args = { "-q", "-" },
+      -- https://github.com/iamcco/diagnostic-languageserver/wiki/Formatters
+      formatters = {
+        black = {
+          command = "black",
+          args = { "-q", "-" },
+        },
+        isort = {
+          command = "isort",
+          args = { "-q", "-" },
+        },
+        shfmt = {
+          command = "shfmt",
+        },
+        stylua = {
+          command = "stylua",
+          args = { "-s", "-" },
+        },
       },
-      shfmt = {
-        command = "shfmt",
+      formatFiletypes = {
+        lua = "stylua",
+        python = { "isort", "black" },
+        sh = "shfmt",
+        zsh = "shfmt",
       },
-      stylua = {
-        command = "stylua",
-        args = { "-s", "-" },
-      },
-    },
-    formatFiletypes = {
-      lua = "stylua",
-      python = { "isort", "black" },
-      sh = "shfmt",
-      zsh = "shfmt",
     },
   },
-})
--- }}}
+  gopls = {},
+  jedi_language_server = {},
+  rust_analyzer = {},
+  sumneko_lua = function()
+    local cmd
+    if vim.fn.executable("lua-language-server") > 0 then
+      cmd = { "lua-language-server" }
+    else
+      local system_name
+      if vim.fn.has("mac") > 0 then
+        system_name = "macOS"
+      elseif vim.fn.has("unix") > 0 then
+        system_name = "Linux"
+      elseif vim.fn.has("win32") > 0 then
+        system_name = "Windows"
+      else
+        print("Unsupported system")
+        return {}
+      end
 
--- {{{ Lua language server
-local cmd
-if vim.fn.executable("lua-language-server") > 0 then
-  cmd = { "lua-language-server" }
-else
-  local system_name
-  if vim.fn.has("mac") > 0 then
-    system_name = "macOS"
-  elseif vim.fn.has("unix") > 0 then
-    system_name = "Linux"
-  else
-    system_name = "Windows"
+      local sumneko_root = vim.fn.stdpath("cache") .. "/lspconfig/sumneko_lua/lua-language-server"
+      local sumneko_bin = sumneko_root .. "/bin/" .. system_name .. "/lua-language-server"
+
+      cmd = { sumneko_bin, "-E", sumneko_root .. "/main.lua" }
+    end
+
+    local runtime_path = vim.split(package.path, ";")
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+
+    return {
+      cmd = cmd,
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT", path = runtime_path },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+        },
+      },
+    }
+  end,
+  texlab = {
+    settings = {
+      texlab = {
+        chktex = { onOpenAndSave = true },
+      },
+    },
+  },
+}
+
+for server, config in pairs(servers) do
+  if type(config) == "function" then
+    config = config()
   end
-
-  local sumneko_root = vim.fn.stdpath("cache") .. "/lspconfig/sumneko_lua/lua-language-server"
-  local sumneko_bin = sumneko_root .. "/bin/" .. system_name .. "/lua-language-server"
-
-  cmd = { sumneko_bin, "-E", sumneko_root .. "/main.lua" }
+  config.on_attach = on_attach
+  config.capabilities = capabilities
+  config.flags = { debounce_text_changes = 100 }
+  lspconfig[server].setup(config)
 end
-
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-lspconfig.sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = cmd,
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT", path = runtime_path },
-      diagnostics = { globals = { "vim" } },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-    },
-  },
-})
 -- }}}
