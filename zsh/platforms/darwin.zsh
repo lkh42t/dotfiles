e4d40c6cae95b68c59cf25d08017bc962fee8e53
Luc Khai Hai <lkh42t@gmail.com>
2021-09-29 03:06:53 +0900

    [zsh] add support for zsh-completion on mac

--- zsh/platforms/darwin.zsh
+++ zsh/platforms/darwin.zsh
@@ -6,6 +6,8 @@ SYNTAX_HIGHLIGHT_SRC="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-
 
 GIT_PS1_SRC=/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
 
+fpath=($(brew --prefix)/share/zsh-completions $fpath)
+
 unload_variables() {
 	unset LS_COLOR_OPT GREP_COLOR_OPT DIFF_COLOR_OPT
 	unset SYNTAX_HIGHLIGHT_SRC
