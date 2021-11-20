ad91085784e7b2f9736315491e60346b70fe5882
Luc Khai Hai <lkh42t@gmail.com>
2021-11-20 23:26:45 +0900

    [nvim] flake8 and mccabe are duplicated

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -133,6 +133,7 @@ local servers = {
       pylsp = {
         plugins = {
           flake8 = { enabled = true },
+          mccabe = { enabled = false },
           pycodestyle = { enabled = false },
           pyflakes = { enabled = false },
         },
