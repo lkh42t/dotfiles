48eaf6113ceb5666fff80078f92ac63921b69321
Luc Khai Hai <lkh42t@gmail.com>
2021-11-22 14:02:55 +0900

    [nvim] follow upstream change

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -1,5 +1,10 @@
-local function feedkeys(s)
-  vim.fn.feedkeys(vim.api.nvim_replace_termcodes(s, true, true, true), "i")
+local function feedkeys(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local cmp = require("cmp")
@@ -25,28 +30,32 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping({
+      i = cmp.mapping.close(),
+      s = cmp.mapping.abort(),
     }),
-    ["<Tab>"] = function(fallback)
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif vim.fn["vsnip#jumpable"](1) > 0 then
-        feedkeys("<Plug>(vsnip-jump-next)")
+        feedkeys("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif vim.fn["vsnip#jumpable"](-1) > 0 then
-        feedkeys("<Plug>(vsnip-jump-prev)")
+        feedkeys("<Plug>(vsnip-jump-prev)", "")
       else
         fallback()
       end
-    end,
+    end, { "i", "s" }),
   },
 })
