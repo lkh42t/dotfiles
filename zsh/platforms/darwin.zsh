72a21b7798f4b6591ed8de37e7a7c0187ad08b8d
Luc Khai Hai <lkh42t@gmail.com>
2022-01-26 19:58:01 +0900

    [zsh] `ls` and `grep` of macOS now can recieve `--color` option

--- zsh/platforms/darwin.zsh
+++ zsh/platforms/darwin.zsh
@@ -1,5 +1,3 @@
-LS_COLOR_OPT='-G'
-GREP_COLOR_OPT='--color=auto'
 DIFF_COLOR_OPT=''
 
 SYNTAX_HIGHLIGHT_SRC="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
@@ -9,7 +7,7 @@ GIT_PS1_SRC=/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-pr
 fpath=($(brew --prefix)/share/zsh-completions $fpath)
 
 unload_variables() {
-	unset LS_COLOR_OPT GREP_COLOR_OPT DIFF_COLOR_OPT
+	unset DIFF_COLOR_OPT
 	unset SYNTAX_HIGHLIGHT_SRC
 	unset GIT_PS1_SRC
 	unset -f unload_variables
