b31dd98d42363d84c6f6d6eddc0e9be5644c8e5f
Luc Khai Hai <lkh42t@gmail.com>
2022-04-04 17:34:45 +0900

    [zsh] fix an error on macos when brew is not installed

--- zsh/platforms/darwin.zsh
+++ zsh/platforms/darwin.zsh
@@ -1,10 +1,10 @@
 DIFF_COLOR_OPT=''
 
-SYNTAX_HIGHLIGHT_SRC="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+SYNTAX_HIGHLIGHT_SRC=$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 GIT_PS1_SRC=/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
 
-fpath=($(brew --prefix)/share/zsh-completions $fpath)
+hash brew 2>/dev/null && fpath=($(brew --prefix)/share/zsh-completions $fpath)
 
 unload_variables() {
 	unset DIFF_COLOR_OPT
