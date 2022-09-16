5b81dd97dc4544f0a1e877ca989de04c931886f0
Luc Khai Hai <lkh42t@gmail.com>
2022-09-16 22:22:13 +0900

    [zsh] More environment variables for macOS

--- zsh/platforms/darwin.zsh
+++ zsh/platforms/darwin.zsh
@@ -3,11 +3,11 @@ DIFF_COLOR_OPT=''
 GIT_PS1_SRC=/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
 
 if [[ -d /opt/homebrew/bin ]]; then
-	path=(/opt/homebrew/bin $path)
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 
-	SYNTAX_HIGHLIGHT_SRC=$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+	SYNTAX_HIGHLIGHT_SRC=$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-	fpath=($(brew --prefix)/share/zsh/site-functions $(brew --prefix)/share/zsh-completions $fpath)
+	fpath=($HOMEBREW_PREFIX/share/zsh/site-functions $HOMEBREW_PREFIX/share/zsh-completions $fpath)
 fi
 
 unload_variables() {
