3958b82f2415c12352beeac9a789b58ad4509924
Luc Khai Hai <lkh42t@gmail.com>
2023-07-01 19:14:23 +0900

    [zsh] Enable git prompt for Debian

--- zsh/platforms/linux.zsh
+++ zsh/platforms/linux.zsh
@@ -2,11 +2,11 @@ DIFF_COLOR_OPT='--color=auto'
 
 if [[ -f /etc/debian_version ]]; then
 	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+	GIT_PS1_SRC=/usr/lib/git-core/git-sh-prompt
 else
 	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+	GIT_PS1_SRC=/usr/share/git/completion/git-prompt.sh
 fi
-
-GIT_PS1_SRC=/usr/share/git/completion/git-prompt.sh
 
 unload_variables() {
 	unset DIFF_COLOR_OPT
