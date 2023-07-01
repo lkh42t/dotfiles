e5f81a00ec41bdd0eba92009203b2c56b6786715
Luc Khai Hai <lkh42t@gmail.com>
2023-07-01 18:00:36 +0900

    [zsh] Support Debian

--- zsh/platforms/linux.zsh
+++ zsh/platforms/linux.zsh
@@ -1,6 +1,10 @@
 DIFF_COLOR_OPT='--color=auto'
 
-SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -f /etc/debian_version ]]; then
+	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+else
+	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
 
 GIT_PS1_SRC=/usr/share/git/completion/git-prompt.sh
 
