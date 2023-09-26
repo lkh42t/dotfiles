849bdec38e6d92bf8a49b23cde913138c9987f89
Luc Khai Hai <lkh42t@gmail.com>
2023-09-26 14:37:19 +0900

    [nvim] Follow upstream settings for lua-language-server

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -125,26 +125,24 @@ local servers = {
   },
   jsonls = {},
   lua_ls = {
-    settings = {
-      Lua = {
-        runtime = {
-          version = "LuaJIT",
-          path = (function()
-            local runtime_path = vim.split(package.path, ";")
-            table.insert(runtime_path, "lua/?.lua")
-            table.insert(runtime_path, "lua/?/init.lua")
-            return runtime_path
-          end)(),
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          checkThirdParty = false,
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-      },
-    },
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+        client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = { vim.env.VIMRUNTIME },
+            },
+          },
+        })
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      end
+      return true
+    end,
     on_attach = disable_formatter,
   },
   pylsp = {
