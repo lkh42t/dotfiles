78ddfbcf39ac77d19125221c333e83b47740afcf
Luc Khai Hai <lkh42t@gmail.com>
2024-09-11 15:35:04 +0900

    [nvim] Clean up lua_ls settings

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -141,29 +141,17 @@ local servers = {
   lua_ls = {
     settings = {
       Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
         format = {
           enable = false,
         },
+        runtime = {
+          version = "LuaJIT",
+        },
       },
     },
-    on_init = function(client)
-      local path = client.workspace_folders[1].name
-      if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-        client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = { vim.env.VIMRUNTIME },
-            },
-          },
-        })
-        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-      end
-      return true
-    end,
   },
   pyright = {
     capabilities = {
