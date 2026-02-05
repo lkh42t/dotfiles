c34940c7ef46094f4dac052885a85d276d0a2701
Luc Khai Hai <lkh42t@gmail.com>
2026-02-06 00:32:14 +0900

    [nvim] Drop luarc.json support from `lua_ls` configuration

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -160,20 +160,6 @@ local servers = {
         format = {
           enable = false,
         },
-      },
-    },
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if
-          path ~= vim.fn.stdpath("config")
-          and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-        then
-          return
-        end
-      end
-
-      client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
         runtime = {
           version = "LuaJIT",
         },
@@ -181,8 +167,8 @@ local servers = {
           library = { vim.env.VIMRUNTIME },
           -- library = vim.api.nvim_get_runtime_file("", true),
         },
-      })
-    end,
+      },
+    },
   },
   pyright = {
     capabilities = {
