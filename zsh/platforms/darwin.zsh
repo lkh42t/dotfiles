cf3d0fcd0b07dbcb32ef3c368a6c2a9a5c36b412
Luc Khai Hai <lkh42t@gmail.com>
2026-05-07 19:13:27 +0900

    [zsh] Split theme setup code into an autoload function

--- zsh/platforms/darwin.zsh
+++ zsh/platforms/darwin.zsh
@@ -1,11 +1,8 @@
-GIT_PS1_SRC=/Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
-
 if [[ -d /opt/homebrew/bin ]]; then
 	if [[ -z $HOMEBREW_PREFIX ]]; then
 		eval "$(/opt/homebrew/bin/brew shellenv)"
 	fi
 
-	[[ -x $HOMEBREW_PREFIX/bin/git ]] && GIT_PS1_SRC=$HOMEBREW_PREFIX/opt/git/etc/bash_completion.d/git-prompt.sh
 	SYNTAX_HIGHLIGHT_SRC=$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 	fpath=($HOMEBREW_PREFIX/share/zsh/site-functions $HOMEBREW_PREFIX/share/zsh-completions $fpath)
@@ -13,6 +10,5 @@ fi
 
 unload_variables() {
 	unset SYNTAX_HIGHLIGHT_SRC
-	unset GIT_PS1_SRC
 	unset -f unload_variables
 }
--- zsh/platforms/linux.zsh
+++ zsh/platforms/linux.zsh
@@ -1,13 +1,10 @@
 if [[ -f /etc/debian_version ]]; then
 	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-	GIT_PS1_SRC=/usr/lib/git-core/git-sh-prompt
 else
 	SYNTAX_HIGHLIGHT_SRC=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-	GIT_PS1_SRC=/usr/share/git/completion/git-prompt.sh
 fi
 
 unload_variables() {
 	unset SYNTAX_HIGHLIGHT_SRC
-	unset GIT_PS1_SRC
 	unset -f unload_variables
 }
