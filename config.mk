0daae09d436a36fd14f9da332a21d02d98774184
Luc Khai Hai <lkh42t@gmail.com>
2026-09-15 01:54:50 +0900

    [kde] Move KDE-specific variables away from common config

--- config.mk
+++ config.mk
@@ -1,9 +1,6 @@
 XDG_CONFIG_HOME ?= $(HOME)/.config
 XDG_DATA_HOME ?= $(HOME)/.local/share
 
-KWRITECONFIG := kwriteconfig6
-QDBUS := qdbus6
-
 .PHONY: config-home
 config-home:
 	mkdir -p $(XDG_CONFIG_HOME)
